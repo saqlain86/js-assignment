689d6b6bfd027b72febfa18eefa1ed0b4cced5d1
saqlain86 <143820913+saqlain86@users.noreply.github.com>
2023-09-26 13:13:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+
+var originalString = "YourStringHere";
+var newString = originalString.toLowerCase();
+var arr = ["Element1", "Element2", "Element3"];
+var lowercaseElement = arr[0].toLowerCase();
+var someString = "YourStringHere";
+alert(someString.toUpperCase());
+var cityName = "kaRacHi";
+cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+var sameWords = "captain";
+var slicedPortion = sameWords.slice(1, 3);
+var stringLength = someString.length;
+var animal = "elephant";
+var seg = animal.slice(1, 5);
+var str = "YourStringHere";
+var strLength = str.length;
+var input = "YourStringHere";
+var charCount = input.length;
+var slicedString = input.slice(1, -3);
+var text = "To be or not to be.";
+var indx = text.indexOf("be");
+// The value of indx will be 3.
+var text = "To be or not to be.";
+var indx = text.lastIndexOf("be");
+// The value of indx will be 16.
+var text = "go, go, gone!";
+var indx = text.lastIndexOf("go");
+if (str.indexOf(segment) !== -1) {
+    // The segment exists in the string.
+  }
+  var str = "abcde";
+  var characterAtIndex2 = str.charAt(2);
+  // characterAtIndex2 will be "c".
+  var text = "This is a long string.";
+  var cha = text.charAt(9);
+  var str = "YourStringHere";
+  var x = str.charAt(str.length - 1);
+  var input = "YourStringHere";
+  var cha = input.charAt(4);
+  if (str.charAt(2) === 'A') {
+    // The 3rd character is 'A'.
+  }
+  var str = "YourStringHere";
+  var characters = [];
+  for (var i = 0; i < str.length; i++) {
+    characters.push(str.charAt(i));
+  }
+  var reply = "No, I don't want to.";
+  var revisedReply = reply.replace("no", "yes");
+  var str = "1 apple and 2 oranges";
+  var newStr = str.replace("1", "one");
+  var roundedNumber = Math.round(numberToRound);
+                  
